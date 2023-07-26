d2043ca3741f447965499d725024fa21f6e5d595
VJ <vijaybhandari7@outlook.com>
2023-07-26 21:28:22 +0530

    This is a new commit for what I originally planned to be amended

---
+++ pyCLI.py
@@ -0,0 +1,11 @@
+# *** objective:
+# login to Putty
+# type few command to bring down the server
+# Traverse to the particular path
+# Remove the file from the directory
+# Again start the server
+
+import ssh
+# server = ssh.Connection(host='host', username='user', private_key='key_path')
+# result = server.execute('your command')
+
