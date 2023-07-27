33c886ec8a360867fa6ad4cbc5d71d143b1082b6
bhandari7 <vijaybhandari7@outlook.com>
2023-07-27 07:02:37 +0530

    added pyCLI.py

--- pyCLI.py
+++ pyCLI.py
@@ -5,7 +5,21 @@
 # Remove the file from the directory
 # Again start the server
 
-import ssh
+# import ssh
 # server = ssh.Connection(host='host', username='user', private_key='key_path')
 # result = server.execute('your command')
+
+
+from pywinauto.application import Application
+import time
+
+app = Application().Start (cmd_line=u'putty -ssh user_name@10.70.15.175')
+putty = app.PuTTY
+putty.Wait('ready')
+time.sleep(1)
+putty.TypeKeys("password")
+putty.TypeKeys("{ENTER}")
+time.sleep(1)
+putty.TypeKeys("ls")
+putty.TypeKeys("{ENTER}")
 
